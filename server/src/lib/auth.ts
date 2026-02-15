360700e7231e2d9d7281a0f5b84d3a6de59b12f5
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-02-15 12:26:37 +0530

    fix auth for expo

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,8 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../exports/prisma.js";
+import { expo } from "@better-auth/expo";
 
 export const auth = betterAuth({
+    plugins: [expo()],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
@@ -10,4 +12,14 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+
+    trustedOrigins: [
+        "munshi://",
+
+            ...(process.env["NODE_ENV"] === "development" ? [
+            "exp://",                      // Trust all Expo URLs (prefix matching)
+            "exp://**",                    // Trust all Expo URLs (wildcard matching)
+            "exp://192.168.*.*:*/**",      // Trust 192.168.x.x IP range with any port and path
+        ] : [])
+    ]
 });
